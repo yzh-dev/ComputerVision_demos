ca0f2c4ebfc938fdb606ff038b083557e817c1e7
yzh_dev <yaozh163@163.com>
2023-04-13 11:42:45 +0800

    no message

--- DeployYOLOmodel.py
+++ DeployYOLOmodel.py
@@ -1,38 +1,33 @@
 import torch
 import numpy as np
 import cv2
-import pafy
 from time import time
 
 
 class ObjectDetection:
     """
-    Class implements Yolo5 model to make inferences on a youtube video using OpenCV.
+    Class implements Yolo5 model to make inferences on a youtube video using Opencv2.
     """
-    
-    def __init__(self, url, out_file):
+
+    def __init__(self, capture_index):
         """
         Initializes the class with youtube url and output file.
         :param url: Has to be as youtube URL,on which prediction is made.
         :param out_file: A valid output file name.
         """
-        self._URL = url
+        self.capture_index = capture_index
         self.model = self.load_model()
         self.classes = self.model.names
-        self.out_file = out_file
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        print("\n\nDevice Used:",self.device)
+        print("Using Device: ", self.device)
 
-
-    def get_video_from_url(self):
+    def get_video_capture(self):
         """
         Creates a new video streaming object to extract video frame by frame to make prediction on.
         :return: opencv2 video capture object, with lowest quality frame available for video.
         """
-        play = pafy.new(self._URL).streams[-1]
-        assert play is not None
-        return cv2.VideoCapture(play.url)
-
+      
+        return cv2.VideoCapture(self.capture_index)
 
     def load_model(self):
         """
@@ -41,7 +36,6 @@ class ObjectDetection:
         """
         model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
         return model
-
 
     def score_frame(self, frame):
         """
@@ -52,10 +46,8 @@ class ObjectDetection:
         self.model.to(self.device)
         frame = [frame]
         results = self.model(frame)
-     
         labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
         return labels, cord
-
 
     def class_to_label(self, x):
         """
@@ -64,7 +56,6 @@ class ObjectDetection:
         :return: corresponding string label
         """
         return self.classes[int(x)]
-
 
     def plot_boxes(self, results, frame):
         """
@@ -86,33 +77,39 @@ class ObjectDetection:
 
         return frame
 
-
     def __call__(self):
         """
         This function is called when class is executed, it runs the loop to read the video frame by frame,
         and write the output into a new file.
         :return: void
         """
-        player = self.get_video_from_url()
-        assert player.isOpened()
-        x_shape = int(player.get(cv2.CAP_PROP_FRAME_WIDTH))
-        y_shape = int(player.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        four_cc = cv2.VideoWriter_fourcc(*"MJPG")
-        out = cv2.VideoWriter(self.out_file, four_cc, 20, (x_shape, y_shape))
+        cap = self.get_video_capture()
+        assert cap.isOpened()
+      
         while True:
+          
             start_time = time()
-            ret, frame = player.read()
-            if not ret:
-                break
+            
+            ret, frame = cap.read()
+            assert ret
+            
             results = self.score_frame(frame)
             frame = self.plot_boxes(results, frame)
+            
             end_time = time()
-            fps = 1/np.round(end_time - start_time, 3)
-            print(f"Frames Per Second : {fps}")
-            out.write(frame)
-
-
-
+            fps = 1/np.round(end_time - start_time, 2)
+            #print(f"Frames Per Second : {fps}")
+             
+            cv2.putText(image, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+            
+            cv2.imshow('YOLOv5 Detection', frame)
+ 
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+      
+        cap.release()
+        
+        
 # Create a new object and execute.
-detection = ObjectDetection("https://www.youtube.com/watch?v=EXUQnLyc3yE", "video2.avi")
-detection()
+detector = ObjectDetection(capture_index=0)
+detector()
